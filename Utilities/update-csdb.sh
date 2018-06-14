870a0f0d185a0ddfda6a2a2097b43a5b89b1a87b
Brown-LiuZan <brown.liuzan@outlook.com>
2018-06-14 17:53:13 +0800

    The first complete version of update-csdb.sh

--- Utilities/update-csdb.sh
+++ Utilities/update-csdb.sh
@@ -2,7 +2,7 @@
 #Brown@copyright
 #Version: 1.0 
 #Date: 20180614
-#Description: Used to create/update and install specified project cscope database.
+#Description: Used to create/update the cscope database of a special kind of source files under specified diretory.
 
 
 #Defining section of this utility.
@@ -24,6 +24,7 @@ usage ()
 {
     echo -e "Usage:\n\tupdate-csdb.sh <c|b|p> RootDir" 1>&2
     echo -e "Note:\n\tc\tC and C++ source files\n\tb\tbash scripts\n\tp\tPython scripts" 1>&2
+    echo -e "\tRootDir\tthe root directory of target project" 1>&2
     exit 1
 }
 if [[ ${#@} -ne 2 ]]; then
@@ -36,6 +37,9 @@ else
 
     RootDir=$2
     SrcFiles=$(basename ${RootDir}).csf
+    if [[ -f ${TagBuildDir}/${SrcFiles} ]]; then
+        mv ${TagBuildDir}/${SrcFiles} ${TagBuildDir}/${SrcFiles}.bak
+    fi
     case $1 in
     'c')
         echo -e "===>Try to create C/C++ source file list for updating cscope database of $2"
@@ -50,11 +54,43 @@ else
         usage;;
     esac
     if [[ -f ${TagBuildDir}/${SrcFiles} ]]; then
+        if [[ -f ${TagBuildDir}/${SrcFiles}.bak ]]; then
+            rm -f ${TagBuildDir}/${SrcFiles}.bak
+        fi
         ls -lh ${TagBuildDir}/${SrcFiles}
     else
-        echo -e "Failed to create a source file list for cscope.\n" 1>&2
+        echo -e "Failed to create/update a source file list for cscope.\n" 1>&2
         exit 1
     fi
     echo -e "===>Done\n"
 fi
 
+#Generate cscope database file.
+echo "===>Try to generate/update cscope database file."
+CsDbFile=$(basename ${RootDir}).cscope.out
+if [[ -f cscope.out ]]; then
+    rm -f cscope.out
+fi
+if [[ -f ${TagBuildDir}/${CsDbFile} ]]; then
+    mv ${TagBuildDir}/${CsDbFile} ${TagBuildDir}/${CsDbFile}.bak
+fi
+cscope -bqk -i ${TagBuildDir}/${SrcFiles}
+if [[ -f cscope.out ]]; then
+    mv cscope.out ${TagBuildDir}/${CsDbFile}
+    if [[ -f ${TagBuildDir}/${CsDbFile} ]]; then
+        rm -f ${TagBuildDir}/${CsDbFile}.bak
+        ls -lh ${TagBuildDir}/${CsDbFile}
+    fi
+    if [[ -f ${TagBuildDir}/${CsDbFile}.bak ]]; then
+        mv ${TagBuildDir}/${CsDbFile}.bak ${TagBuildDir}/${CsDbFile}
+        echo -e "Failed to generate/udpate cscope database file.\n"
+        exit 1
+    fi
+else
+    if [[ -f ${TagBuildDir}/${CsDbFile}.bak ]]; then
+        mv ${TagBuildDir}/${CsDbFile}.bak ${TagBuildDir}/${CsDbFile}
+    fi
+    echo -e "Failed to generate/update cscope database file.\n"
+    exit 1
+fi
+echo -e "===>Done.\n"
